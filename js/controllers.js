d2d96a4b706fc0c9ab98adf64673a8753bc1d133
Shay <shayhall@nccumc.org>
2015-09-27 21:07:28 -0400

    Last touches

--- js/controllers.js
+++ js/controllers.js
@@ -168,62 +168,79 @@
       }, 500);
       setTimeout(function() {
         $("#d4").addClass("white-pawn");
+      moveCounter = 1;
       }, 1000);
       setTimeout(function() {
         $("#g8").removeClass("black-knight");
       }, 1500);
       setTimeout(function() {
         $("#f6").addClass("black-knight");
+      moveCounter = 2;
+      console.log(moveCounter);
       }, 2000);
       setTimeout(function() {
         $("#c2").removeClass("white-pawn");
       }, 2500);
       setTimeout(function() {
         $("#c4").addClass("white-pawn");
+      moveCounter = 3;
+      console.log(moveCounter);
       }, 3000);
       setTimeout(function() {
         $("#e7").removeClass("black-pawn");
       }, 3500);
       setTimeout(function() {
         $("#e6").addClass("black-pawn");
+        moveCounter = 4;
+        console.log(moveCounter);
       }, 4000);
       setTimeout(function() {
         $("#g2").removeClass("white-pawn");
       }, 4500);
       setTimeout(function() {
         $("#g3").addClass("white-pawn");
+        moveCounter = 5;
+        console.log(moveCounter);
       }, 5000);
       setTimeout(function() {
         $("#d7").removeClass("black-pawn");
       }, 5500);
       setTimeout(function() {
         $("#d5").addClass("black-pawn");
+        moveCounter = 6;
+        console.log(moveCounter);
       }, 6000);
       setTimeout(function() {
         $("#f1").removeClass("white-bishop");
       }, 6500);
       setTimeout(function() {
         $("#g2").addClass("white-bishop");
+        moveCounter = 7;
+        console.log(moveCounter);
       }, 7000);
       setTimeout(function() {
         $("#f8").removeClass("black-bishop");
       }, 7500);
       setTimeout(function() {
         $("#e7").addClass("black-bishop");
+        moveCounter = 8;
+        console.log(moveCounter);
       }, 8000);
       setTimeout(function() {
         $("#g1").removeClass("white-knight");
       }, 8500);
       setTimeout(function() {
         $("#f3").addClass("white-knight");
+        moveCounter = 9;
+        console.log(moveCounter);
       }, 9000);
      // TODO: Tell the Model -- `game` -- to do something it knows how to do...
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
   });
 
-  // Controller for anything else...
+  // Controller for pause...
   jQuery(".fa-pause").on('click'/* dunno */, function(event){
-    alert("pause");
+    return true;
     // TODO: Tell the Model -- `game` -- to do something it knows how to do...
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
   });
